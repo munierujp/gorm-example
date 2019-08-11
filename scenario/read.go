53975178d80dd25500239904a6bb8868efefc548
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-11 22:22:09 +0900

    Update Read scenario

--- scenario/read.go
+++ scenario/read.go
@@ -2,7 +2,6 @@ package scenario
 
 import (
 	"fmt"
-	"gorm-example/domain/repository"
 	"gorm-example/interface/database"
 
 	"github.com/jinzhu/gorm"
@@ -11,20 +10,22 @@ import (
 func Read(db *gorm.DB) error {
 	userRepo := database.NewUserRepository(db)
 
-	if err := findByID(userRepo, 4); err != nil {
+	fmt.Println("> Get last record")
+	user, err := userRepo.Last()
+	if err != nil {
 		return err
 	}
+	fmt.Println("id" + "\t" + "name" + "\t" + "language_id" + "\t" + "created_at" + "\t" + "updated_at" + "\t" + "deleted_at")
+	fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n", user.ID, user.Name, user.LanguageID, user.CreatedAt, user.UpdatedAt, user.DeletedAt)
+
+	id := user.ID
+	fmt.Printf("> Find by id (%v)\n", id)
+	user, err = userRepo.FindByID(id)
+	if err != nil {
+		return err
+	}
+	fmt.Println("id" + "\t" + "name" + "\t" + "language_id" + "\t" + "created_at" + "\t" + "updated_at" + "\t" + "deleted_at")
+	fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n", user.ID, user.Name, user.LanguageID, user.CreatedAt, user.UpdatedAt, user.DeletedAt)
 
 	return nil
-}
-
-func findByID(userRepo repository.UserRepository, id uint) error {
-	fmt.Printf("> Find by id (%v)\n", id)
-	if user, err := userRepo.FindByID(id); err != nil {
-		return err
-	} else {
-		fmt.Println("id" + "\t" + "name" + "\t" + "language_id" + "\t" + "created_at" + "\t" + "updated_at" + "\t" + "deleted_at")
-		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\n", user.ID, user.Name, user.LanguageID, user.CreatedAt, user.UpdatedAt, user.DeletedAt)
-		return nil
-	}
 }
